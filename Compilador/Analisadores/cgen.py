46d5802c42748dd13ceab2a5be874622beb6d558
LeonardoEnne12 <enneleonardo@gmail.com>
2022-05-23 19:00:19 -0300

    Gerador Quase pronto

--- Compilador/Analisadores/cgen.py
+++ Compilador/Analisadores/cgen.py
@@ -44,7 +44,6 @@ This part of the code build the code generator python
 nextDec = 0
 label = 0
 temp = 0
-quadList = []
 flag = 0
 tempAux = ""
 tempAux2 = ""
@@ -53,9 +52,8 @@ def newTemp():
     global temp
     temp = (temp + 1)%17
 
-def genStatement(tree):
+def genStatement(tree,quadList):
     name = tree.name.split()[0]
-    global quadList
     global label
     global flag
     global temp
@@ -64,16 +62,16 @@ def genStatement(tree):
 
     if name == 'If':
         if(len(tree.children) > 0):
-            cGen(tree.children[0])
+            cGen(tree.children[0],quadList)
         arg1 = tempAux
         arg2 = f"L{label}"
         aux1 = arg2
         arg3 = "-"
         label = label + 1
-        quadList.append("IFF",arg1,arg2,arg3)
+        quadList.append(f"IFF {arg1} {arg2} {arg3}")
 
         if(len(tree.children) > 1):
-            cGen(tree.children[1])
+            cGen(tree.children[1],quadList)
         
         if(len(tree.children) > 2):
             arg1 = f"L{label}"
@@ -81,14 +79,14 @@ def genStatement(tree):
             arg2 = "-"
             arg3 = "-"
             label = label + 1
-            quadList.append("GOTO",arg1,arg2,arg3)
+            quadList.append(f"GOTO {arg1} {arg2} {arg3}")
 
         if(len(tree.children) > 2):
             arg1 = aux1
             arg2 = "-"
             arg3 = "-"
-            quadList.append("LABEL",arg1,arg2,arg3)
-            cGen(tree.children[2])
+            quadList.append(f"LABEL {arg1} {arg2} {arg3}")
+            cGen(tree.children[2],quadList)
 
         if(len(tree.children) > 2):
             arg1 = aux2
@@ -97,20 +95,20 @@ def genStatement(tree):
         
         arg2 = "-"
         arg3 = "-"
-        quadList.append("LABEL",arg1,arg2,arg3)
+        quadList.append(f"LABEL {arg1} {arg2} {arg3}")
 
 
     elif name == 'Atribuicao':
         if(len(tree.children) > 0):
             if(tree.children[0].name.split()[0] == 'Vetor:'):
                 flag = 1
-            cGen(tree.children[0])
+            cGen(tree.children[0],quadList)
         arg1 = tempAux
         if(len(tree.children) > 1):
-            cGen(tree.children[1])
+            cGen(tree.children[1],quadList)
         arg2 = tempAux
         arg3 = "-"
-        quadList.append("ASSIGN",arg1,arg2,arg3)
+        quadList.append(f"ASSIGN {arg1} {arg2} {arg3}")
 
         if(flag == 2):
             arg3 = tempAux2
@@ -120,7 +118,7 @@ def genStatement(tree):
         if(len(tree.children) > 0):
             arg1 = tree.children[0].name.split()[1]
         arg2 = aux1
-        quadList.append("STORE",arg1,arg2,arg3)
+        quadList.append(f"STORE {arg1} {arg2} {arg3}")
 
     elif name == 'While':
         arg1 = f"L{label}"
@@ -128,40 +126,40 @@ def genStatement(tree):
         aux1 = arg1;
         arg2 = "-"
         arg3 = "-";
-        quadList.append("LABEL",arg1,arg2,arg3)
+        quadList.append(f"LABEL {arg1} {arg2} {arg3}")
 
         if(len(tree.children) > 0):
-            cGen(tree.children[0])
+            cGen(tree.children[0],quadList)
         arg1 = tempAux
         arg2 = f"L{label}"
         label = label + 1
         aux2 = arg2;
         arg3 = "-";
-        quadList.append("IFF",arg1,arg2,arg3)
+        quadList.append(f"IFF {arg1} {arg2} {arg3}")
         
         if(len(tree.children) > 1):
-            cGen(tree.children[1])
+            cGen(tree.children[1],quadList)
         arg1 = aux1
         arg2 = "-"
         arg3 = "-";
-        quadList.append("GOTO",arg1,arg2,arg3)
+        quadList.append(f"GOTO {arg1} {arg2} {arg3}")
 
         arg1 = aux2
         arg2 = "-"
         arg3 = "-";
-        quadList.append("GOTO",arg1,arg2,arg3)
+        quadList.append(f"GOTO {arg1} {arg2} {arg3}")
 
     elif name == 'Variavel:': #Rever essa parte 
         arg1 = tree.name.split()[1]
         arg2 = tree.name.split()[2]
         if tree.name.split()[3] == 2:
             arg3 = tree.name.split()[4]
-            quadList.append("ALLOC",arg1,arg2,arg3)
+            quadList.append(f"ALLOC {arg1} {arg2} {arg3}")
             if arg2 == "global": 
                 globalSize = tree.name.split()[4]
         else:
             arg3 = "-"
-            quadList.append("ALLOC",arg1,arg2,arg3)
+            quadList.append(f"ALLOC {arg1} {arg2} {arg3}")
             if arg2 == "global":
                 globalSize = 1
 
@@ -172,35 +170,35 @@ def genStatement(tree):
             arg1 = 'void'
         arg2 = tree.name.split()[1]
         arg3 = '-'
-        quadList.append("GOTO",arg1,arg2,arg3)
+        quadList.append(f"GOTO {arg1} {arg2} {arg3}")
         if(len(tree.children) > 0):
-            cGen(tree.children[0])
+            cGen(tree.children[0],quadList)
         if(len(tree.children) > 1):    
-            cGen(tree.children[1])
+            cGen(tree.children[1],quadList)
 
         arg1 = arg2
         arg2 = '-'
         arg3 = '-'
-        quadList.append("END",arg1,arg2,arg3)
+        quadList.append(f"END {arg1} {arg2} {arg3}")
 
     elif name == 'Chamada':
         if(len(tree.children) > 0):
-            cGen(tree.children[0])
+            cGen(tree.children[0],quadList)
 
         tempAux = f"$t{temp}"
         newTemp()
         arg1 = tempAux
-        arg2 = tree.name
+        arg2 = tree.name.split()[3]
         arg3 = "param counter que não sei o é"
-        quadList.append("CALL",arg1,arg2,arg3)
+        quadList.append(f"CALL {arg1} {arg2} {arg3}")
 
     elif name == 'Return':
         if(len(tree.children) > 0):
-            cGen(tree.children[0])
+            cGen(tree.children[0],quadList)
         arg1 = tempAux
         arg2 = '-'
         arg3 = '-'
-        quadList.append("RET",arg1,arg2,arg3)
+        quadList.append(f"RET {arg1} {arg2} {arg3}")
     
     elif name == 'Parametro:':
         if tree.name.split()[3] == 1:
@@ -209,13 +207,12 @@ def genStatement(tree):
             arg1 = "int[]"
         arg2 = tree.name.split()[1]
         arg3 = tree.name.split()[2]
-        quadList.append("ARG",arg1,arg2,arg3)
+        quadList.append(f"ARG {arg1} {arg2} {arg3}")
 
 
 
-def genExpression(tree):
+def genExpression(tree,quadList):
     name = tree.name.split()[0]
-    global quadList
     global label
     global flag
     global temp
@@ -224,44 +221,44 @@ def genExpression(tree):
 
     if name == 'Operacao:':
         if(len(tree.children) > 0):
-            cGen(tree.children[0])
+            cGen(tree.children[0],quadList)
         arg2 = tempAux
         if(len(tree.children) > 1):
-            cGen(tree.children[1])
+            cGen(tree.children[1],quadList)
         arg3 = tempAux;
         tempAux = f"$t{temp}"
         newTemp()
         arg1 = tempAux
 
         if tree.name.split()[-1] == '+':
-            quadList.append("ADD",arg1,arg2,arg3)
+            quadList.append(f"ADD {arg1} {arg2} {arg3}")
         
         elif tree.name.split()[-1] == '-':
-            quadList.append("SUB",arg1,arg2,arg3)
+            quadList.append(f"SUB {arg1} {arg2} {arg3}")
         
         elif tree.name.split()[-1] == '*':
-            quadList.append("MUL",arg1,arg2,arg3)
+            quadList.append(f"MUL {arg1} {arg2} {arg3}")
         
         elif tree.name.split()[-1] == '/':
-            quadList.append("DIV",arg1,arg2,arg3)
-        
+            quadList.append(f"DIV {arg1} {arg2} {arg3}")
+         
         elif tree.name.split()[-1] == '<': 
-            quadList.append("LT",arg1,arg2,arg3)
+            quadList.append(f"LT {arg1} {arg2} {arg3}")
         
         elif tree.name.split()[-1] == '<=': 
-            quadList.append("LET",arg1,arg2,arg3)
+            quadList.append(f"LET {arg1} {arg2} {arg3}")
         
         elif tree.name.split()[-1] == '>': 
-            quadList.append("GT",arg1,arg2,arg3)
+            quadList.append(f"GT {arg1} {arg2} {arg3}")
         
         elif tree.name.split()[-1] == '>=':
-            quadList.append("GET",arg1,arg2,arg3)
+            quadList.append(f"GET {arg1} {arg2} {arg3}")
         
         elif tree.name.split()[-1] == '==':
-            quadList.append("EQ",arg1,arg2,arg3)
+            quadList.append(f"EQ {arg1} {arg2} {arg3}")
         
         elif tree.name.split()[-1] == '!=': 
-            quadList.append("NEQ",arg1,arg2,arg3)
+            quadList.append(f"NEQ {arg1} {arg2} {arg3}")
         
 
     elif name == 'Constante:':
@@ -270,7 +267,7 @@ def genExpression(tree):
         arg1 = tempAux
         arg2 = tree.name.split()[1]
         arg3 = "-"
-        quadList.append("LOAD",arg1,arg2,arg3)
+        quadList.append(f"LOAD {arg1} {arg2} {arg3}")
     
     elif name == 'Id:':
         tempAux = f"$t{temp}"
@@ -278,11 +275,11 @@ def genExpression(tree):
         arg1 = tempAux
         arg2 = tree.name.split()[1]
         arg3 = "-"
-        quadList.append("LOAD",arg1,arg2,arg3)
+        quadList.append(f"LOAD {arg1} {arg2} {arg3}")
 
     elif name == 'Vetor:':
         if(len(tree.children) > 0):
-            cGen(tree.children[0])
+            cGen(tree.children[0],quadList)
         
         arg3 = tempAux
         if (flag == 1):
@@ -293,35 +290,37 @@ def genExpression(tree):
         newTemp()
         arg1 = tempAux
         arg2 = tree.name.split()[1]
-        quadList.append("LOAD",arg1,arg2,arg3)
+        quadList.append(f"LOAD {arg1} {arg2} {arg3}")
 
-    elif name == 'Tipo':
+    elif name == 'Tipo:':
         if(len(tree.children) > 0):
-            cGen(tree.children[0])
+            cGen(tree.children[0],quadList)
     
     
-def cGen(tree):
+def cGen(tree,quadList):
 
     if (tree != None):
         global nextDec 
         nextDec = nextDec + 1
         name = tree.name.split()[0]
         statem = ['If','Atribuicao','While','Variavel:','Funcao:','Chamada','Return','Parametro:']
-        expre = ['Operacao:','Constante:','Id:','Vetor:','Index','Tipo']
+        expre = ['Operacao:','Constante:','Id:','Vetor:','Index','Tipo:']
         if name in statem:
-            genStatement(tree)
+            genStatement(tree,quadList)
         elif name in expre:
-            genExpression(tree)
+            genExpression(tree,quadList)
     
     nextDec = nextDec - 1
     if (nextDec == 0):
         if(len(tree.children) > 1):
-            cGen(tree.children[1])
+            cGen(tree.children[1],quadList)
 
+quadruLista = []
+cGen(root,quadruLista)
 
-cGen(root)
+for quadrupla in quadruLista:
+    print(quadrupla)
 
-print(quadList)
 
 #last function to work        
 def quadList(tree):
